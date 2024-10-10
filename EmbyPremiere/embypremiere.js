889b9ef982934010612a16bdfb7fe376e24dd122
RoiexLee <2648281162@xiaomi.com>
2024-10-10 13:43:57 +0800

    commit

--- EmbyPremiere/embypremiere.js
+++ EmbyPremiere/embypremiere.js
@@ -1,4 +1,3 @@
-$notification.post("Emby Premiere Script Start!");
 if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') !== -1) {
     if ($response.status !== 200) {
         $notification.post("Emby Premiere Unlocked!");
@@ -8,11 +7,8 @@ if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice
             body: '{"cacheExpirationDays": 999, "resultCode": "GOOD", "message": "Device Valid", "isPremiere": true}'
         });
     } else {
-        $notification.post("Emby Premiere Expired!");
         $done({});
     }
 } else {
-    $notification.post("Emby Premiere Error!");
     $done({});
 }
-$notification.post("Emby Premiere Script End!");
